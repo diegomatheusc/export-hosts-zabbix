3d3b160694ae0aca41878f21516d28400ba450e3
Diego Matheus <60781404+diegomatheusc@users.noreply.github.com>
2023-07-08 19:45:12 -0300

    Update export-zabbix.py

--- export-zabbix.py
+++ export-zabbix.py
@@ -3,8 +3,8 @@ import json
 
 # Configurações do Zabbix
 url = 'http://seu_zabbix/api_jsonrpc.php'
-user = 'usuario'
-password = 'senha'
+user = 'seu_usuario'
+password = 'sua_senha'
 
 # Função para fazer uma requisição à API do Zabbix
 def make_request(data):
@@ -32,7 +32,11 @@ def export_hosts(token):
         'jsonrpc': '2.0',
         'method': 'host.get',
         'params': {
-            'output': 'extend'
+            'output': 'extend',
+            'selectGroups': 'extend',
+            'selectParentTemplates': ['templateid', 'name'],
+            'selectItems': ['itemid', 'name', 'key_'],
+            'selectTriggers': ['triggerid', 'description']
         },
         'auth': token,
         'id': 1
@@ -43,9 +47,22 @@ def export_hosts(token):
 # Faz login e obtém o token de autenticação
 token = login()
 
-# Exporta todos os hosts
+# Exporta todos os hosts com grupos, templates, itens e triggers
 hosts = export_hosts(token)
 
-# Exibe os hosts exportados
+# Exibe os hosts exportados com detalhes
 for host in hosts:
-    print(host)
+    print('Host:', host['host'])
+    print('Groups:')
+    for group in host['groups']:
+        print(' -', group['name'])
+    print('Templates:')
+    for template in host['parentTemplates']:
+        print(' -', template['name'])
+    print('Items:')
+    for item in host['items']:
+        print(' -', item['name'], '(' + item['key_'] + ')')
+    print('Triggers:')
+    for trigger in host['triggers']:
+        print(' -', trigger['description'])
+    print('------------------')
